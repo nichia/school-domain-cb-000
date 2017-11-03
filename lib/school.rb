add3e08c796d9e51ceeca29c240b98641a113f66
Ni Chia <nichia@gmail.com>
2017-11-03 15:42:24 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+
+class School
+  attr_reader :school
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << student_name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    result = {}
+    @roster.each { |grade, student_name|
+      result[grade] = student_name.sort
+    }
+    result
+  end
+end
